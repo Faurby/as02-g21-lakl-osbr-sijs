89379b12d642dd5a8905c47880d5458bce9788a6
Simon Johann Skødt <simon@skodt.dk>
2021-09-20 14:31:59 +0200

    Finished exercise 1

---
+++ StudentLibrary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StudentLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var StartDate = new DateTime(2020, 8, 10);
+            var EndDate = new DateTime(2025, 05, 28);
+            var GraduationDate = new DateTime(2025, 05, 28);
+            Student student = new Student(4317, "Simon Johann", "Skødt", StartDate, EndDate, GraduationDate);
+            
+            Console.WriteLine(student.ToString());
+        }
+    }
+}
---
+++ StudentLibrary/Student.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace StudentLibrary
+{
+    public class Student 
+    {
+        int _Id;
+        string _GivenName;
+        string _SurName;
+        public enum Status {
+            New,
+            Active,
+            Dropout,
+            Graduated
+        }
+        DateTime _StartDate;
+        DateTime _EndDate;
+        DateTime _GraduationDate;
+
+        // Properties
+        public int Id 
+        { 
+            get => _Id; 
+            // init Only to be set in constructor
+        }
+        public string GivenName 
+        {
+            get => _GivenName;
+            set => _GivenName = value;
+        }
+        public string SurName 
+        {
+            get => _SurName;
+            set => _SurName = value;
+        }
+        public DateTime StartDate 
+        {
+            get => _StartDate;
+            set => _StartDate = value; 
+        }
+        public DateTime EndDate
+        {
+            get => _EndDate;
+            set => _EndDate = value; 
+        }
+        public DateTime GraduationDate
+        {
+            get => _GraduationDate;
+            set => _GraduationDate = value; 
+        }
+
+        public Student(int Id, string GivenName, string SurName, DateTime StartDate, DateTime EndDate, DateTime GraduationDate) 
+        {
+            _Id = Id; // Only set in constructor
+            _GivenName = GivenName;
+            _SurName = SurName;
+            _StartDate = StartDate;
+            _EndDate = EndDate;
+            _GraduationDate = GraduationDate;
+        }
+
+        public Status BusinessRules() 
+        {
+            // DateTime: Year, month, day
+            DateTime localDate = DateTime.Now;
+
+            var localStart = DateTime.Compare(localDate, StartDate);
+            var localGraduation = DateTime.Compare(localDate, GraduationDate);
+            var localEnd = DateTime.Compare(localDate, EndDate);
+            var endGraduation = DateTime.Compare(EndDate, GraduationDate);
+
+            // New student
+            if (localStart == -1) 
+            {
+                return Status.New;
+            }
+            // Active student
+            else if (localStart == 1 && localEnd == -1)
+            {
+                return Status.Active; 
+            }
+            // Dropout
+            else if (endGraduation == -1 && localEnd == 1) 
+            {
+                return Status.Dropout;
+            }
+            // Graduated
+            else 
+            {
+                return Status.Graduated;
+            }
+        }
+
+        public override string ToString() 
+        {
+            return $"Id: {Id}, {GivenName} {SurName}, Status: {BusinessRules()}, Start date: {StartDate}, End date: {EndDate}, Graduation date: {GraduationDate}";
+        }
+    }
+}
