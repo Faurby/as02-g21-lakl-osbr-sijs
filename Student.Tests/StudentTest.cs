c5c58e8992c42b59ecf7f485bf1eb35ec6d54dc6
Simon Johann Skødt <simon@skodt.dk>
2021-09-20 11:08:03 +0200

    Started implementing getters, setters and constructor

---
+++ Student.Tests/StudentTest.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+
+namespace Student.Tests
+{
+    public class StudentTest
+    {
+        [Fact]
+        public void Checks_if_status_is_only_readable()
+        {
+            
+        }
+
+        public void Id_only_accessed_when_Student_is_instantiated() 
+        {
+
+        }
+
+        public void ToString_returns_expected_string() 
+        {
+            
+        }
+    }
+}
--- Student/Student.cs
+++ Student/Student.cs
@@ -4,26 +4,35 @@ namespace StudentRelated
     public class Student 
     {
         int _Id;
-        string GivenName;
-        string Surname;
+        string _GivenName;
+        string _Surname;
         enum Status {
             New,
             Active,
             Dropout,
             Graduated
         }
-        DateTime StartDate;
-        DateTime EndDate;
-        DateTime GraduationDate;
+        DateTime _StartDate;
+        DateTime _EndDate;
+        DateTime _GraduationDate;
 
-        public Student(int Id) 
+        public string _GivenName { get; set; }
+        public string _SurName { get; set; }
+        public string _StartDate { get; set; }
+        public string _EndDate { get; set; }
+        public string _GraduationDate { get; set; }
+
+        public Student(int Id, string GivenName, string SurName, DateTime StartDate, DateTime EndDate, DateTime GraduationDate) 
         {
             _Id = Id;
+            
         }
 
-        public string ToString() 
+        public override string ToString() 
         {
             
         }
+
+        public 
     }
 }
